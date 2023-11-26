df3bf576fbf0ef7ce1ff018692e41d4fbeada811
dimirob <82870059+dimirob@users.noreply.github.com>
2023-11-26 12:42:26 +0200

    Final commit (added comments)

--- Exercise5/sign-up.js
+++ Exercise5/sign-up.js
@@ -21,14 +21,14 @@ function buttons(){//function for questionaire buttons
     var btns2=prev_use.getElementsByClassName("basic_paragraph")//2 choices
     for(var i=0;i<btns1.length;i++){//find buttons-choices
             btns1[i].addEventListener("click", function() {//event listener
-            current = post_buy.getElementsByClassName("active");
+            current = post_buy.getElementsByClassName("active");//get buttons that already active so they can be replaced
             if(current.length>0){
                 current[0].className = current[0].className.replace("active", "basic_paragraph");//make other buttons inactive
             }
             this.className="active";//button was clicked, so make it active
         });
     }
-    for(var i=0;i<btns2.length;i++){
+    for(var i=0;i<btns2.length;i++){//copy from top
         btns2[i].addEventListener("click", function() {
         current = prev_use.getElementsByClassName("active");
         if(current.length>0){
@@ -38,7 +38,7 @@ function buttons(){//function for questionaire buttons
         });
     }
 }
-function checkTel(){
+function checkTel(){//function to check if the mobile phone number is valid (has 10 digits)
     let tel=document.getElementById('cellnum')
     tel.onchange=function(){
         if(!(tel.value>6900000000&&tel.value<6999999999)){
@@ -49,11 +49,11 @@ function checkTel(){
         }
     }
 }
-function checkpass(){
+function checkpass(){//function to check if passwords match and if the password is 8 characters long
     let pass=document.getElementById("password")
     let passc=document.getElementById("passwordc")
     pass.onchange=function(){
-        if(pass.value.length<8){
+        if(pass.value.length<8){//check length
             pass.setCustomValidity("Password must be at least 8 characters long")
         }
         else{
@@ -61,7 +61,7 @@ function checkpass(){
         }
     }
     passc.onchange=function(){
-        if(!(pass.value==passc.value)){
+        if(!(pass.value==passc.value)){//check similarity
             passc.setCustomValidity("Passwords dont match")
         }
         else{
@@ -69,18 +69,18 @@ function checkpass(){
         }
     }
 }
-function checkDate(){
+function checkDate(){//func to check if user is over 18 years old 
     var today=new Date()
     let dob=document.getElementById("dob")
     dob.onchange=function(){
-        var year=dob.value.substring(0,4)
+        var year=dob.value.substring(0,4)//extract year from string 
         
-        var month=dob.value.substring(5,7)
+        var month=dob.value.substring(5,7)//extract month from string 
         
-        var day=dob.value.substring(8,10)
+        var day=dob.value.substring(8,10)//extract day from string 
         
         var dateob=new Date(year,month,day)
-        var yearsold=Math.floor((today - dateob) / 31556952000)
+        var yearsold=Math.floor((today - dateob) / 31556952000)//convert to years
         if(yearsold<18){
             dob.setCustomValidity("Sorry, you can not be under 18 years old")
         }
